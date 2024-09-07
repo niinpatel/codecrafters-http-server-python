2eb4d77c7cbf4cf3a2c67905c74f2cf429e39c9c
niinpatel <nitinpatel278@gmail.com>
2024-09-07 06:56:24 +0530

    handle request

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
 import socket
 
 
+def handle_request(client_socket: socket.socket):
+    request = client_socket.recv(1024)
+
+    method = request.decode().split(" ")[0]
+    path = request.decode().split(" ")[1]
+
+    if method == "GET" and path == "/":
+        client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    client_socket.close()
+
+
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, _ = server_socket.accept()
-    client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    client_socket.close()
+
+    while True:
+        client_socket, _ = server_socket.accept()
+        handle_request(client_socket)
 
 
 if __name__ == "__main__":
