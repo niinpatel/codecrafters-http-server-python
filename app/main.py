51f4f7f693c9f2fe21ad0b984fe856eee4af5b6c
niinpatel <nitinpatel278@gmail.com>
2024-09-07 07:13:20 +0530

    implement user agent parsing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 import socket
 
 
+def parse_headers(request: str) -> dict:
+    headers_strings = request.decode().split("\r\n")[1:-2]
+    print(headers_strings)
+    headers = {}
+    for line in headers_strings:
+        if line:
+            key, value = line.split(": ", 1)
+            headers[key] = value
+    return headers
+
+
 def handle_request(client_socket: socket.socket):
     request = client_socket.recv(1024)
 
@@ -14,6 +25,13 @@ def handle_request(client_socket: socket.socket):
         message = path.split("/echo/")[1]
         client_socket.sendall(
             f"HTTP/1.1 200 OK\r\nContent-Length: {len(message)}\r\nContent-Type: text/plain\r\n\r\n{message}".encode()
+        )
+
+    elif method == "GET" and path.startswith("/user-agent"):
+        headers = parse_headers(request)
+        user_agent = headers.get("User-Agent", "")
+        client_socket.sendall(
+            f"HTTP/1.1 200 OK\r\nContent-Length: {len(user_agent)}\r\nContent-Type: text/plain\r\n\r\n{user_agent}".encode()
         )
 
     else:
