68ab9e883a68d445d6a64fa2c60677047ce60dbb
niinpatel <nitinpatel278@gmail.com>
2024-09-07 06:53:41 +0530

    first test done

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-import socket  # noqa: F401
+import socket
 
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    client_socket, _ = server_socket.accept()
+    client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    client_socket.close()
 
 
 if __name__ == "__main__":
